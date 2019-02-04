015d31dfe59c53aa4cc7614e6ffe9cb52d6446b5
Akhtar <reachtoakhtar@gmail.com>
2019-02-04 08:59:03 +0530

    - Implemented routing in the app.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,16 +2,11 @@
  * Created by Akhtar on 3/Jan/19.
  */
 
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
